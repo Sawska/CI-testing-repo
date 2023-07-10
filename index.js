f7455d722199ad0466145cd14aaeb9cfaea82366
Alexander Korzh <alexanderkorzh@MacBook-Air-Alexander.local>
2023-07-10 19:00:29 +0300

    fixed test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express  = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-
-app.listen(3000,"localhost")
+app.listen(3000, "localhost", () => {
+  console.log("Server started on port 3000");
+});
